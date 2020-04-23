a93a4d73ba6ab7423223a37768303c50d6e916f4
Sean McDonald <spmcdonald1316@gmail.com>
2020-04-23 17:15:13 -0500

    add show action for players

--- app/controllers/api/players_controller.rb
+++ app/controllers/api/players_controller.rb
@@ -3,4 +3,12 @@ class Api::PlayersController < ApplicationController
     @players = Player.where("last_name LIKE ?", "#{params[:search]}")
     render 'index.json.jb'
   end
+
+  def show
+    @player = Player.find_by(id: params[:id])
+    @hitting = HittingStat.where(player_id: @player.player_id)
+    @pitching = PitchingStat.where(player_id: @player.player_id)
+    @fielding = FieldingStat.where(player_id: @player.player_id)
+    render 'show.json.jb'
+  end
 end
