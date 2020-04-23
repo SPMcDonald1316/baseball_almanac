2e863526f13e9c4afc908c0d579739888b6afa08
Sean McDonald <spmcdonald1316@gmail.com>
2020-04-23 08:55:11 -0500

    generate team stats table and code seed for teams csv

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_23_124225) do
+ActiveRecord::Schema.define(version: 2020_04_23_133959) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -135,6 +135,59 @@ ActiveRecord::Schema.define(version: 2020_04_23_124225) do
     t.string "bbref_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "team_stats", force: :cascade do |t|
+    t.integer "year_id"
+    t.string "lg_id"
+    t.string "team_id"
+    t.string "franch_id"
+    t.string "div_id"
+    t.integer "rank"
+    t.integer "g"
+    t.integer "g_home"
+    t.integer "w"
+    t.integer "l"
+    t.string "div_win"
+    t.string "wc_win"
+    t.string "lg_win"
+    t.string "ws_win"
+    t.integer "r"
+    t.integer "ab"
+    t.integer "h"
+    t.integer "doubles"
+    t.integer "triples"
+    t.integer "hr"
+    t.integer "bb"
+    t.integer "so"
+    t.integer "sb"
+    t.integer "cs"
+    t.integer "hbp"
+    t.integer "sf"
+    t.integer "ra"
+    t.integer "er"
+    t.decimal "era", precision: 3, scale: 2
+    t.integer "cg"
+    t.integer "sho"
+    t.integer "sv"
+    t.integer "ip_outs"
+    t.integer "ha"
+    t.integer "bba"
+    t.integer "soa"
+    t.integer "e"
+    t.integer "dp"
+    t.decimal "fp", precision: 4, scale: 3
+    t.string "name"
+    t.string "park"
+    t.integer "attendance"
+    t.integer "bpf"
+    t.integer "ppf"
+    t.string "team_id_br"
+    t.string "team_id_lahman"
+    t.string "team_id_retro"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "hra"
   end
 
 end
