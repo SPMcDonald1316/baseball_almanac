a5246977bf6950626c83e9b70b293cd819cb018c
Sean McDonald <spmcdonald1316@gmail.com>
2020-04-26 09:34:48 -0500

    update players show action and view to render post season stats

--- app/controllers/api/players_controller.rb
+++ app/controllers/api/players_controller.rb
@@ -7,8 +7,10 @@ class Api::PlayersController < ApplicationController
   def show
     @player = Player.find_by(id: params[:id])
     @hitting = HittingStat.where(player_id: @player.player_id)
+    @post_hitting = PostHittingStat.where(player_id: @player.player_id)
     @pitching = PitchingStat.where(player_id: @player.player_id)
     @fielding = FieldingStat.where(player_id: @player.player_id)
+    @post_fielding = PostFieldingStat.where(player_id: @player.player_id)
     render 'show.json.jb'
   end
 end
