59feae92618390ff223cf454c4d2da0676c5a8d7
Sean McDonald <spmcdonald1316@gmail.com>
2020-05-04 14:51:01 -0500

    reformat player debut and final game

--- app/models/player.rb
+++ app/models/player.rb
@@ -2,4 +2,12 @@ class Player < ApplicationRecord
   def fullname
     "#{first_name} #{last_name}"
   end
+
+  def debut_friendly
+    debut.strftime("%m-%d-%Y") unless debut.nil?
+  end
+
+  def final_friendly
+    final_game.strftime("%m-%d-%Y") unless final_game.nil?
+  end
 end
