210bd2a2354b62486e02ab444d4917ae7f9ff3e8
Sean McDonald <spmcdonald1316@gmail.com>
2020-05-09 18:49:47 -0500

    start model method for play by play data

--- app/models/play_by_play_two_thousand_ten.rb
+++ app/models/play_by_play_two_thousand_ten.rb
@@ -1,2 +1,12 @@
 class PlayByPlayTwoThousandTen < ApplicationRecord
+  def home_team
+    team_abbr = game_id[0..2]
+    team = Franchise.find_by(franch_id: team_abbr)
+    home_team = team.franch_name
+  end
+
+  def away_team
+    team = Franchise.find_by(franch_id: away_team_id)
+    away_team = team.franch_name
+  end
 end
