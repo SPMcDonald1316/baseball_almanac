1f88c3776ce8e41803dfcea3e18312e8e192d94a
Sean McDonald <spmcdonald1316@gmail.com>
2020-05-12 20:22:11 -0500

    add post pitching stats to show action and json

--- app/controllers/api/players_controller.rb
+++ app/controllers/api/players_controller.rb
@@ -1,6 +1,6 @@
 class Api::PlayersController < ApplicationController
   def index
-    @players = Player.where("last_name LIKE ?", "#{params[:search]}%")
+    @players = Player.select(:id, :first_name, :last_name, :debut, :final_game).where("last_name LIKE ?", "#{params[:search]}%")
     render 'index.json.jb'
   end
 
@@ -9,7 +9,7 @@ class Api::PlayersController < ApplicationController
     @hitting = HittingStat.where(player_id: @player.player_id)
     @post_hitting = PostHittingStat.where(player_id: @player.player_id)
     @pitching = PitchingStat.where(player_id: @player.player_id)
-    # @post_pitching = PitchingStat.where(player_id: @player.player_id)
+    @post_pitching = PostPitchingStat.where(player_id: @player.player_id)
     @fielding = FieldingStat.where(player_id: @player.player_id)
     @post_fielding = PostFieldingStat.where(player_id: @player.player_id)
     render 'show.json.jb'
